28de8a11ced8ababd38f89c1699831cda6babade
36riya <124116965+36riya@users.noreply.github.com>
2023-05-02 17:34:49 -0400

    Add files via upload

---
+++ cs410_6A/include/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define SSID "Apt314"
+#define PASSWORD "Wifinotworking"
+// #define SSID "Home_abd"
+// #define PASSWORD "Avenue@77"
+// #define SSID "Riya's iphone"
+// #define PASSWORD "riya3601"
