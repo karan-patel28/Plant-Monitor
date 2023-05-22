4b37f01d2ea8b9a99a9aa67f7cecfab2dc424337
Karan Patel <k.patel021@umb.edu>
2023-05-22 00:04:22 -0400

    delete old files

--- esp32 c++/cs410_6A/include/constants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define SSID "Apt314"
-#define PASSWORD "Wifinotworking"
-// #define SSID "Home_abd"
-// #define PASSWORD "Avenue@77"
-// #define SSID "Riya's iphone"
-// #define PASSWORD "riya3601"
